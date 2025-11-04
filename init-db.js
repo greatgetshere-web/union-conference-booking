890da8056ae0381a55f852ffd7ce74e0c8a5d0ce
greatgetshere-web <greatgetshere@gmail.com>
2025-11-04 01:33:42 +0100

    Update init-db.js

--- init-db.js
+++ init-db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, '../data/bookings.db');
+const dbPath = '/tmp/bookings.db';
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
